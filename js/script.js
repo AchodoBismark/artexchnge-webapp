87503a85637e0765ccf440fc688b6cc4a799825a
Bismark Achodo <achodobismark@gmail.com>
2020-08-07 21:35:45 +0100

    worked on exchange page

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@ $("ul.nav-tabs a").click(function (e) {
     e.preventDefault();  
       $(this).tab('show');
   });
+  import {MDCTextField} from '@material/textfield';
+  const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
