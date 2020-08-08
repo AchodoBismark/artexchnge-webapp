619e0033ea9a4bc0d2750ec54e4437fa577592fb
Bismark Achodo <achodobismark@gmail.com>
2020-08-08 14:26:44 +0100

    started work on listings

--- js/script.js
+++ js/script.js
@@ -2,5 +2,3 @@ $("ul.nav-tabs a").click(function (e) {
     e.preventDefault();  
       $(this).tab('show');
   });
-  import {MDCTextField} from '@material/textfield';
-  const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
